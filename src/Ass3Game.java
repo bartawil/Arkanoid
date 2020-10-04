8d29d994a89a26882ab1c1897acb381f171ac2ec
bar <barrtawil@gmail.com>
2020-10-04 10:23:41 +0300

    ass3

---
+++ src/Ass3Game.java
@@ -0,0 +1,16 @@
+/**
+ * a Game class.
+ *
+ * @author Bar Tawil
+ */
+public class Ass3Game {
+    /**
+     * run the game.
+     * @param args - none
+     */
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.initialize();
+        game.run();
+    }
+}
