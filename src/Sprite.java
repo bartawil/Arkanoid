c139b9b4bba5a813641bd1412b2506dc1d6b7775
bar <barrtawil@gmail.com>
2020-10-04 10:15:37 +0300

    ass3

---
+++ src/Sprite.java
@@ -0,0 +1,19 @@
+import biuoop.DrawSurface;
+
+/**
+ * This interface will be used by things that should be draw on.
+ *
+ * @author Bar Tawil
+ */
+public interface Sprite {
+
+    /**
+     * @param d - draw the sprite to the screen.
+     */
+    void drawOn(DrawSurface d);
+
+    /**
+     * notify the sprite that time has.
+     */
+    void timePassed();
+}
