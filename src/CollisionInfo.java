0f1c94b7f30b2177d8460b767e465b4b0e7dbc3c
bar <barrtawil@gmail.com>
2020-10-04 10:13:48 +0300

    ass3

---
+++ src/CollisionInfo.java
@@ -0,0 +1,34 @@
+/**
+ * A CollisionInfo class.
+ *
+ * @author Bar Tawil
+ */
+public class CollisionInfo {
+    private Point collisionPoint;
+    private Collidable collidable;
+
+    /**
+     * Construct the collisionInfo by given 'collidable' object and collision point.
+     *
+     * @param collisionPoint - collision point
+     * @param collidable     - block
+     */
+    public CollisionInfo(Point collisionPoint, Collidable collidable) {
+        this.collisionPoint = collisionPoint;
+        this.collidable = collidable;
+    }
+
+    /**
+     * @return - the point where the collision occurs.
+     */
+    public Point collisionPoint() {
+        return this.collisionPoint;
+    }
+
+    /**
+     * @return - the 'collidable' object involved in the collision.
+     */
+    public Collidable collisionObject() {
+        return this.collidable;
+    }
+}
